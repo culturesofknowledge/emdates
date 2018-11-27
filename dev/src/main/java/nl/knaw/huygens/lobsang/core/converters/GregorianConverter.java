47b2f884ef213322df109c6240ed273b57470886
Martijn Maas <martijn.maas@huygens.knaw.nl>
2018-11-27 11:25:52 +0100

    Implement Gregorian.fromRataDie
    A modulo function is added that will always return a positive value.
    This function behaves more like the Lisp mod function.

--- dev/src/main/java/nl/knaw/huygens/lobsang/core/converters/GregorianConverter.java
+++ dev/src/main/java/nl/knaw/huygens/lobsang/core/converters/GregorianConverter.java
@@ -5,12 +5,14 @@ import nl.knaw.huygens.lobsang.api.YearMonthDay;
 import static java.lang.Math.floor;
 
 public class GregorianConverter implements CalendarConverter {
+  private static final int LAST_DAY_LEAP_YEAR_400 = 146097;
+  private static final int LAST_DAY_LEAP_YEAR_4 = 1461;
   private static final int GREGORIAN_EPOCH = 1; // definition from Calendrical Calculations
 
   @Override
   public int toRataDie(YearMonthDay date) {
     int year = date.getYear();
-    return (int) (GREGORIAN_EPOCH - 1 + 365 * (year - 1) + floor((double)(year - 1) / 4) -
+    return (int) (GREGORIAN_EPOCH - 1 + 365 * (year - 1) + floor((double) (year - 1) / 4) -
         floor((double) (year - 1) / 100) + floor((double) (year - 1) / 400) +
         floor((double) (367 * date.getMonth() - 362) / 12) + getLeapNumber(date) + date.getDay());
   }
@@ -28,11 +30,59 @@ public class GregorianConverter implements CalendarConverter {
   }
 
   private boolean isGregorianLeapYear(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    return (mod(year, 4) == 0 && mod(year, 100) != 0) || mod(year, 400) == 0;
   }
 
   @Override
   public YearMonthDay fromRataDie(int rataDie) {
-    throw new UnsupportedOperationException("Yet to be implemented");
+    int year = gregorianYearFromRataDie(rataDie);
+    int priorDays = rataDie - gregorianNewYear(year);
+    int correction = getCorrection(rataDie, year);
+    int month = (int) floor((double) ( (12 * (priorDays + correction)) + 373) / 367);
+    int day = rataDie - toRataDie(new YearMonthDay(year, month, 1)) + 1;
+
+    return new YearMonthDay(year, month, day);
+  }
+
+  private int getCorrection(int rataDie, int year) {
+    if (rataDie < toRataDie(new YearMonthDay(year, 3, 1))) {
+      return 0;
+    }
+
+    if (isGregorianLeapYear(year)) {
+      return 1;
+    }
+
+    return 2;
+  }
+
+  private int gregorianNewYear(int year) {
+    return toRataDie(new YearMonthDay(year, 1, 1));
+  }
+
+  private int gregorianYearFromRataDie(int rataDie) {
+    int priorDays = rataDie - GREGORIAN_EPOCH;
+    int completed400YearCycles = (int) floor((double) priorDays / LAST_DAY_LEAP_YEAR_400);
+    int priorDaysNotIn400 = mod(priorDays, LAST_DAY_LEAP_YEAR_400);
+    int cycle100YearNotIn400 = (int) floor((double) priorDaysNotIn400 / 36524);
+    int priorDaysNotIn400Or100 = mod(priorDaysNotIn400, 36524);
+    int cycle4YearNotIn400Or100 = (int) floor((double) priorDaysNotIn400Or100 / LAST_DAY_LEAP_YEAR_4);
+    int priorDaysNotIn400Or100Or4 = mod(priorDaysNotIn400Or100, LAST_DAY_LEAP_YEAR_4);
+    int yearsNotIn400Or100Or4 = (int) floor((double) priorDaysNotIn400Or100Or4 / 365);
+
+    int year = 400 * completed400YearCycles + 100 * cycle100YearNotIn400 + 4 * cycle4YearNotIn400Or100 +
+        yearsNotIn400Or100Or4;
+    return cycle100YearNotIn400 == 4 || yearsNotIn400Or100Or4 == 4 ? year : year + 1;
+  }
+
+  // mod function that will always return a positive value, that is more like the lisp mod function
+  // see: https://stackoverflow.com/a/4403642
+  // see: http://jtra.cz/stuff/lisp/sclr/mod.html
+  private int mod(int value, int modBase) {
+    while (value < 0) {
+      value += modBase;
+    }
+    int mod = value % modBase;
+    return mod;
   }
 }
--- dev/src/test/java/nl/knaw/huygens/lobsang/core/converters/GregorianConverterTest.java
+++ dev/src/test/java/nl/knaw/huygens/lobsang/core/converters/GregorianConverterTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,48 +21,52 @@ public class GregorianConverterTest {
     instance = new GregorianConverter();
   }
 
+  private static Function<TestData.TestRow, Arguments> mapData() {
+    return row -> Arguments.of(row.getGregorian(), row.getRataDie());
+  }
+
   private static Stream<Arguments> data2019() throws Exception {
-    return TestData.getData("2019.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2019.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2018() throws Exception {
-    return TestData.getData("2018.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2018.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2017() throws Exception {
-    return TestData.getData("2017.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2017.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2016() throws Exception {
-    return TestData.getData("2016.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2016.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2015() throws Exception {
-    return TestData.getData("2015.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2015.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2014() throws Exception {
-    return TestData.getData("2014.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2014.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2013() throws Exception {
-    return TestData.getData("2013.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2013.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2012() throws Exception {
-    return TestData.getData("2012.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2012.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2011() throws Exception {
-    return TestData.getData("2011.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2011.csv").map(mapData());
   }
 
   private static Stream<Arguments> data2010() throws Exception {
-    return TestData.getData("2010.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("2010.csv").map(mapData());
   }
 
   private static Stream<Arguments> dataDates1() throws Exception {
-    return TestData.getData("dates1.csv").map(row -> Arguments.of(row.getGregorian(), row.getRataDie()));
+    return TestData.getData("dates1.csv").map(mapData());
   }
 
   @ParameterizedTest
@@ -72,6 +77,16 @@ public class GregorianConverterTest {
     int rataDie = instance.toRataDie(gregorianDate);
 
     assertThat(rataDie, is(expectedRataDie));
+  }
+
+  @ParameterizedTest
+  @MethodSource({"data2019", "data2018", "data2017", "data2016", "data2015", "data2014", "data2013", "data2012",
+      "data2011", "data2010", "dataDates1"
+  })
+  public void testFromRataDie(YearMonthDay expectedGregorianDate, int rataDie) {
+    YearMonthDay gregorian = instance.fromRataDie(rataDie);
+
+    assertThat(gregorian, is(expectedGregorianDate));
   }
 
 }
