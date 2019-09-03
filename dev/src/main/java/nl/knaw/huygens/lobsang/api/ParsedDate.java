efac20d5f1b80ac05288fbc71cdfec496d85e7de
HDJ <hayco@users.noreply.github.com>
2019-09-03 13:13:09 +0200

    Expose parsing notes to front end

--- dev/src/main/java/nl/knaw/huygens/lobsang/api/ParsedDate.java
+++ dev/src/main/java/nl/knaw/huygens/lobsang/api/ParsedDate.java
@@ -2,22 +2,25 @@ package nl.knaw.huygens.lobsang.api;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Optional;
+
 public class ParsedDate {
   @JsonProperty
   private final String parsedDate;
   @JsonProperty
+  private final String notes;
+  @JsonProperty
   private final String errorMessage;
 
   private ParsedDate(YearMonthDay parsedDate, String errorMessage) {
-
     this.parsedDate = parsedDate.asIso8601String();
+    this.notes = Optional.ofNullable(parsedDate.getNotes()).map(Object::toString).orElse(null);
     this.errorMessage = errorMessage;
   }
 
   public static ParsedDate forDate(YearMonthDay parsedDate) {
     return new ParsedDate(parsedDate, null);
   }
-
 
   public static ParsedDate forError(String message) {
     return new ParsedDate(null, message);
