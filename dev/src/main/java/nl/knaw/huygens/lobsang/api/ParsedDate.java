6222c8af7c8e7bdbeed2d957dcf6041cf76dfa98
HDJ <hayco@users.noreply.github.com>
2019-09-04 15:30:45 +0200

    Properly display parsing notes as list

--- dev/src/main/java/nl/knaw/huygens/lobsang/api/ParsedDate.java
+++ dev/src/main/java/nl/knaw/huygens/lobsang/api/ParsedDate.java
@@ -3,18 +3,19 @@ package nl.knaw.huygens.lobsang.api;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Optional;
+import java.util.Set;
 
 public class ParsedDate {
   @JsonProperty
   private final String parsedDate;
   @JsonProperty
-  private final String notes;
+  private final Set<String> notes;
   @JsonProperty
   private final String errorMessage;
 
   private ParsedDate(YearMonthDay parsedDate, String errorMessage) {
     this.parsedDate = parsedDate.asIso8601String();
-    this.notes = Optional.ofNullable(parsedDate.getNotes()).map(Object::toString).orElse(null);
+    this.notes = parsedDate.getNotes();
     this.errorMessage = errorMessage;
   }
 
